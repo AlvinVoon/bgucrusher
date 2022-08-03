1b9a0908fd49b5d2aa0048610bcf0c714ee50287
AlvinVoon <62414477+AlvinVoon@users.noreply.github.com>
2022-08-03 15:57:08 +0800

    moving elephant

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("tree").style.opacity = "0";
+//document.getElementById("tree").style.opacity = "0";
 const img = document.createElement("img");
 img.src = "./tree.png";
 const src = document.getElementById("character");
@@ -6,3 +6,13 @@ function change(){
     document.getElementById("seed").style.opacity = "0";
     document.getElementById("tree").style.opacity = "1";
 }
+
+const elephant=document.getElementById("character")
+let left_value = 0;
+
+function move(){
+    left_value += 0.05;
+    elephant.style.left = `${left_value}%`;
+}
+
+setInterval(move, 50 )
